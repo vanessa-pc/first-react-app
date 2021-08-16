822221c7d564d0314a9c7c366a94f1149469aa01
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-16 18:14:30 +0300

    Change img src to local files

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,5 +1,10 @@
 import Place from "./Place";
 
+import kyoto from '../../src/images/kyoto.jpg'
+import seljalandsfoss from '../../src/images/seljalandsfoss.jpeg'
+import santorini from '../../src/images/santorini.jpeg'
+import veniceBeach from '../../src/images/venice_beach.jpeg'
+
 function MainContent(): JSX.Element {
   return (
     <section>
@@ -7,9 +12,7 @@ function MainContent(): JSX.Element {
         title={"A bamboo trail in Kyoto"}
         placeName={"Kyoto"}
         countryName={"Japan"}
-        mainImage={
-          "https://images.unsplash.com/photo-1607619662634-3ac55ec0e216?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80"
-        }
+        mainImage={kyoto}
         locationLink={
           "https://www.google.com/maps/place/Kyoto,+Japan/@35.0978739,135.438682,10z/data=!3m1!4b1!4m5!3m4!1s0x6001a8d6cd3cc3f1:0xc0961d366bbb1d3d!8m2!3d35.011564!4d135.7681489"
         }
@@ -19,9 +22,7 @@ function MainContent(): JSX.Element {
         title={"Seljalandsfoss, A waterfall in Iceland"}
         placeName={"Seljalandsfoss"}
         countryName={"Iceland"}
-        mainImage={
-          "https://images.unsplash.com/photo-1506261423908-ea2559c1f24c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1062&q=80"
-        }
+        mainImage={seljalandsfoss}
         locationLink={
           "https://www.google.com/maps/place/Seljalandsfoss,+Iceland/data=!4m2!3m1!1s0x48d71eade8ef2415:0xae01e6205209178d?sa=X&ved=2ahUKEwiTm5vFlq7yAhWjnFwKHSD7C28Q8gF6BAgYEAE"
         }
@@ -31,9 +32,7 @@ function MainContent(): JSX.Element {
         title={"Sunset at Venice Beach"}
         placeName={"Los Angeles"}
         countryName={"USA"}
-        mainImage={
-          "https://images.unsplash.com/photo-1523595857-fe9ee689f76f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-        }
+        mainImage={veniceBeach}
         locationLink={
           "https://www.google.com/maps/@33.9874948,-118.4967346,13z"
         }
@@ -45,9 +44,7 @@ function MainContent(): JSX.Element {
         title={"Santorini views"}
         placeName={"Santorini"}
         countryName={"Greece"}
-        mainImage={
-          "https://images.unsplash.com/photo-1517926967795-31943e805dae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-        }
+        mainImage={santorini}
         locationLink={"https://www.google.com/maps/@36.4203259,25.4234157,15z"}
         text={
           "A stairway with a panorama over the Aegean sea in Thera Santorini."
